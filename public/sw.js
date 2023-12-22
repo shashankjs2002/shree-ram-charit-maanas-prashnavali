286a66576c17cfdb2bda73bdaf9f1ce150a07675
shashankjs2002 <shashankjs2002@gmail.com>
2023-12-22 18:04:38 +0530

    Service worker

--- public/sw.js
+++ public/sw.js
@@ -1,28 +1,56 @@
 let cacheData = "prashanavali";
 
+let files = [
+    '/static/js/bundle.js',
+    '/',
+    'manifest.json',
+    'favicon.ico',
+    'logo192.png',
+    '/ws',
+
+    '/static/js/787.2c48817c.chunk.js',
+    '/static/js/787.2c48817c.chunk.js.map',
+    '/static/js/main.d7f334e7.js',
+    '/static/js/main.d7f334e7.js.LICENSE.txt',
+    '/static/js/main.d7f334e7.js.map',
+    '/static/css/main.98b8690a.css',
+    '/static/css/main.98b8690a.css.map',
+
+
+
+
+]
 this.addEventListener("install", (event) => {
+
+
     event.waitUntil(
-        caches.open(cacheData).then((cache) => {
-            cache.addAll([
-                '/static/js/bundle.js',
-                '/',
-                'manifest.json',
-                'favicon.ico',
-                'logo192.png',
-                '/ws',
+        caches.open(cacheData).then(async (cache) => {
+            // cache.addAll([
+            //     '/static/js/bundle.js',
+            //     '/',
+            //     'manifest.json',
+            //     'favicon.ico',
+            //     'logo192.png',
+            //     '/ws',
 
-                '/static/js/787.2c48817c.chunk.js',
-                '/static/js/787.2c48817c.chunk.js.map',
-                '/static/js/main.d7f334e7.js',
-                '/static/js/main.d7f334e7.js.LICENSE.txt',
-                '/static/js/main.d7f334e7.js.map',
-                '/static/css/main.98b8690a.css',
-                '/static/css/main.98b8690a.css.map',
-                
+            //     '/static/js/787.2c48817c.chunk.js',
+            //     '/static/js/787.2c48817c.chunk.js.map',
+            //     '/static/js/main.d7f334e7.js',
+            //     '/static/js/main.d7f334e7.js.LICENSE.txt',
+            //     '/static/js/main.d7f334e7.js.map',
+            //     '/static/css/main.98b8690a.css',
+            //     '/static/css/main.98b8690a.css.map',            
+            // ])
 
-
-            
-            ])
+            for (let i of files) {
+                try {
+                  let ok = await cache.add(i);
+                  
+                } catch (err) {
+                  console.warn('sw: cache.add',i);
+                }
+              }
+        
         })
     )
 })
