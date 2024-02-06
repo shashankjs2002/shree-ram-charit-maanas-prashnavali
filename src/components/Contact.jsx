a33197b3854f83703f991c5189134710adf71cea
shashankjs2002 <shashankjs2002@gmail.com>
2024-02-07 00:19:25 +0530

    Footer

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -57,7 +57,7 @@ const Contact = () => {
                                     <p>
                                         {item.desc}
                                     </p>
-                                    <a href={item.link.href} target="_blank" rel="noreferrer" className="flex items-center gap-1 text-sm text-indigo-600 duration-150 hover:text-indigo-400 font-medium">
+                                    <a href={item.link.href} target="_blank" rel="noreferrer" className="flex items-center gap-1 text-sm text-orange-500 duration-150 hover:text-orange-400 font-medium">
                                         {item.link.name}
                                         <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" className="w-5 h-5">
                                             <path fillRule="evenodd" d="M5 10a.75.75 0 01.75-.75h6.638L10.23 7.29a.75.75 0 111.04-1.08l3.5 3.25a.75.75 0 010 1.08l-3.5 3.25a.75.75 0 11-1.04-1.08l2.158-1.96H5.75A.75.75 0 015 10z" clipRule="evenodd" />
